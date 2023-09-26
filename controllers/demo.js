3fd0bf9750d6cb1c874275bd5a9db98da7e76254
Rocio Woscoff <rociowoscoff3@gmail.com>
2023-09-26 18:24:48 -0300

    commit final

--- controllers/demo.js
+++ controllers/demo.js
@@ -118,9 +118,39 @@ const getGeneros = (req = request, res = response) => {
 
 }
 
+const getFiltrarGeneroID = (req = request, res = response) => {
+    const api = process.env.API_KEY;
+    const {id} = req.params;
+    console.log(id);
+        
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${api}&with_genres={id}`)
+        
+        .then(( {status, data, statusText}) => {
+            // handle success
+            console.log({status, data, statusText});
+            const{results,page} = data;
+            if (results.length === 0){
+                res.status(404).json({msg : 'Pagina no encontrada'});
+            }else{
+            res.status(200).json({
+                status,
+                results,
+                statusText,
+            });
+            }
+        })
+            
+        .catch( (error) => {
+            // handle error
+            console.log(error);
+            res.status(401).json({msg: 'error insesperado'}) 
+        });
+
+}
 module.exports = {
     getEstrenos, 
     getAnio, 
     getTendencias, 
-    getGeneros
+    getGeneros,
+    getFiltrarGeneroID
 }
--- routes/demo.js
+++ routes/demo.js
@@ -1,5 +1,5 @@
 const {Router} = require('express');
-const { getEstrenos, getAnio, getTendencias, getGeneros } = require('../controllers/demo');
+const { getEstrenos, getAnio, getTendencias, getGeneros, getFiltrarGeneroID } = require('../controllers/demo');
 
 
 const rutas = Router();
@@ -8,10 +8,7 @@ rutas.get('/estrenos', getEstrenos);
 rutas.get('/anio/:id', getAnio);
 rutas.get('/tendencias', getTendencias);
 rutas.get('/generos', getGeneros); 
+rutas.get('/generoID/:id', getFiltrarGeneroID);
 
 
-<<<<<<< HEAD
 module.exports = rutas;
-=======
-module.exports = rutas;
->>>>>>> f7e13924a5e06f9e8985663427150a8983c5525f
