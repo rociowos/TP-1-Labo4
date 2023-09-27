093bfba878bc276351d44eb3647ac30ff17a80a4
Rocio Woscoff <rociowoscoff3@gmail.com>
2023-09-27 15:12:29 -0300

    ultimo commit pt2

--- controllers/demo.js
+++ controllers/demo.js
@@ -35,7 +35,7 @@ const getAnio = (req = request, res = response) => {
     const {id} = req.params;
     console.log(id);
         
-        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${api}&year={id}`)
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${api}&primary_release_year={id}`)
         
         .then(( {status, data, statusText}) => {
             // handle success
@@ -92,31 +92,28 @@ const getTendencias = (req = request, res = response) => {
 const getGeneros = (req = request, res = response) => {
     const api = process.env.API_KEY;
         
-        axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${api}`)
-        
-        .then(( {status, data, statusText}) => {
+    axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${api}`)
+        .then(({ status, data, statusText }) => {
             // handle success
-            console.log({status, data, statusText});
-            const{results,page} = data;
-            if (results.length === 0){
-                res.status(404).json({msg : 'Pagina no encontrada'});
-            }else{
-            res.status(200).json({
-                status,
-                page,
-                results,
-                statusText,
-            });
+            console.log({ status, data, statusText });
+            const { genres } = data;
+            if (!genres || genres.length === 0) {
+                res.status(404).json({ msg: 'Pagina no encontrada' });
+            } else {
+                res.status(200).json({
+                    status,
+                    genres,
+                    statusText,
+                });
             }
         })
-            
-        .catch( (error) => {
+        .catch((error) => {
             // handle error
             console.log(error);
-            res.status(401).json({msg: 'error insesperado'}) 
+            res.status(500).json({ msg: 'Error inesperado' });
         });
-
 }
+
 
 const getFiltrarGeneroID = (req = request, res = response) => {
     const api = process.env.API_KEY;
